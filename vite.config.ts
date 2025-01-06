a90b94c0ea7accc26b44b01728774a3f581a6ef3
Ahmad Kholmakhmatov <ahmadxolmaxmatov@gmail.com>
2025-01-06 10:51:56 +0500

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://67.205.184.13:8000', // Replace with your backend URL
-        changeOrigin: true,
-        secure: false, // Set to true if using HTTPS on your backend
-      },
-    },
-  },
+
 });
